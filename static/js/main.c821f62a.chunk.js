266b1b38e06fe52d1904a2441741d9197c8e29af
Tetiana Blizniukova <tatbliz30@gmail.com>
2021-10-11 19:14:14 +0300

    Updates

--- static/js/main.c821f62a.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-02-phonebook"]=this["webpackJsonpgoit-react-hw-02-phonebook"]||[]).push([[0],{11:function(e,t,n){"use strict";n.r(t);var o=n(1),c=n.n(o),r=n(3),i=n.n(r),s=(n(8),n(9),n(0));var h=function(){return Object(s.jsx)("div",{className:"App",children:"hw-02-phonebook"})};i.a.render(Object(s.jsx)(c.a.StrictMode,{children:Object(s.jsx)(h,{})}),document.getElementById("root"))},8:function(e,t,n){},9:function(e,t,n){}},[[11,1,2]]]);
-//# sourceMappingURL=main.c821f62a.chunk.js.map
